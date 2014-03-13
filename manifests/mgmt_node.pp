5cda1a5cfdca54ac18569e6499db268fe1ab2be1
Douglas McClusky <drmcclus@ncsu.edu>
2014-03-13 08:03:43 -0400

    configuring site table according to mgmtconf.sh from records.

--- manifests/mgmt_node.pp
+++ manifests/mgmt_node.pp
@@ -1,6 +1,6 @@
 include vclmgmt
 
-define vclmgmt::mgmt_node($public_mac, $public_if = 'em1', $public_ip = 'dhcp', $private_mac, $private_ip = '172.20.0.1', $private_net = '172.20.0.0', $private_mask = '255.252.0.0', $private_if = 'em2', $ipmi_mac, $ipmi_ip = '172.25.0.1', $ipmi_net = '172.24.0.0', $ipmi_mask = '255.252.0.0', $ipmi_if = 'p4p1', $vcldb = 'vcl', $vcluser = 'vcluser', $root_pw, $vcluser_pw, $vclhost = 'localhost', $serverip = 'localhost', $xmlrpc_pw = 'just_another_password', $xml_url = 'localhost', $ipmi_user, $ipmi_pw, $admin_user, $admin_pw) {
+define vclmgmt::mgmt_node($public_mac, $public_if = 'em1', $public_ip = 'dhcp', $private_mac, $private_ip = '172.20.1.1', $private_net = '172.20.1.0', $private_mask = '255.252.1.0', $private_if = 'em2', $ipmi_mac, $ipmi_ip = '172.24.1.1', $ipmi_net = '172.24.1.0', $ipmi_mask = '255.255.254.0', $ipmi_range = '172.24.101.1-172.24.1.204', $ipmi_if = 'p4p1', $vcldb = 'vcl', $vcluser = 'vcluser', $root_pw, $vcluser_pw, $vclhost = 'localhost', $serverip = 'localhost', $xmlrpc_pw = 'just_another_password', $xml_url = 'localhost', $ipmi_user, $ipmi_pw, $admin_user, $admin_pw) {
 	vclmgmt::networks { "mgmt_interfaces" :
 		public_mac 	=> $public_mac,
 		public_if	=> $public_if, 
@@ -37,6 +37,7 @@ define vclmgmt::mgmt_node($public_mac, $public_if = 'em1', $public_ip = 'dhcp', 
                 ipmi_pw		=> $ipmi_pw,
 		ipmi_net	=> $ipmi_net,
 		ipmi_mask	=> $ipmi_mask,
+		ipmi_range	=> $ipmi_range,
                 admin_user	=> $admin_user,
                 admin_pw	=> $admin_pw,
 		private_if	=> $private_if,
--- manifests/xcat_init.pp
+++ manifests/xcat_init.pp
@@ -1,6 +1,6 @@
 include vclmgmt
 
-define vclmgmt::xcat_init($ipmi_ip, $ipmi_net, $ipmi_mask, $ipmi_if, $ipmi_user, $ipmi_pw, $admin_user, $admin_pw, $private_net, $private_mask, $private_if)  {
+define vclmgmt::xcat_init($ipmi_ip, $ipmi_net, $ipmi_mask, $ipmi_range, $ipmi_if, $ipmi_user, $ipmi_pw, $admin_user, $admin_pw, $private_net, $private_mask, $private_if)  {
     xcat_node { "ipmi" :
         bmc => "/\\\\z/-ipmi/",
         bmcusername => $ipmi_user,
@@ -12,7 +12,12 @@ define vclmgmt::xcat_init($ipmi_ip, $ipmi_net, $ipmi_mask, $ipmi_if, $ipmi_user,
 
     xcat_site_attribute { "master" :
         sitename => 'clustersite',
-        value => $ipmi_ip,
+        value => $fqdn,
+    }
+
+    xcat_site_attribute { "nameservers" :
+        sitename => 'clustersite',
+        value => $private_ip,
     }
 
     xcat_site_attribute { "dhcpinterfaces" :
@@ -20,10 +25,30 @@ define vclmgmt::xcat_init($ipmi_ip, $ipmi_net, $ipmi_mask, $ipmi_if, $ipmi_user,
 	value => [$private_if, $ipmi_if],
     }
 
+    xcat_site_attribute { "domain" :
+        sitename => 'clustersite',
+        value => 'netlabs.cluster',
+    }
+
+    xcat_site_attribute { "ntpservers" :
+       	sitename => 'clustersite',
+       	value => 'time.ncsu.edu',
+    }
+
+    xcat_site_attribute { "xcatroot" :
+       	sitename => 'clustersite',
+        value => "/opt/xcat",
+    }
+
+    xcat_site_attribute	{ "xcatprefix" :
+        sitename => 'clustersite',
+       	value => "/opt/xcat",
+    }
+
     xcat_network { "private" :
         mgtifname => $private_if,
-       	nameservers => "<xcatmaster>",
-       	gateway	=> "<xcatmaster>",
+       	nameservers => "xcatmaster",
+       	gateway	=> "xcatmaster",
        	domain => "private.netlabs",
        	net => $private_net,
        	mask =>	$private_mask,
@@ -31,10 +56,11 @@ define vclmgmt::xcat_init($ipmi_ip, $ipmi_net, $ipmi_mask, $ipmi_if, $ipmi_user,
 
     xcat_network { "ipmi" :
        	mgtifname => $ipmi_if,
-        nameservers => "<xcatmaster>",
-        gateway => "<xcatmaster>",
+        nameservers => "xcatmaster",
+        gateway => "xcatmaster",
         domain => "ipmi.netlabs",
         net => $ipmi_net,
         mask => $ipmi_mask,
+        dynamicrange => $ipmi_range,
     }
 }
