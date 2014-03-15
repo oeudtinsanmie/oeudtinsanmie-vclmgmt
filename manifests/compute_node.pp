34270fc8210ed2f66ac443ea5acaddfc6b9ab810
Douglas McClusky <drmcclus@ncsu.edu>
2014-03-15 00:55:41 -0400

    fix variable names in compute_node

--- manifests/compute_node.pp
+++ manifests/compute_node.pp
@@ -1,23 +1,23 @@
 include vclmgmt
 
-define vclmgmt::compute_node($node, $private_servname, $private_ip, $private_mac, $private_if, $ipmi_ip, $ipmi_mac, $ipmi_if, $ipmi_user, $ipmi_pw, $tgt_if, $tgt_os = 'Linux', $tgt_arch = 'x86_64', $slotid, $admin_user, $admin_pw) {
+define vclmgmt::compute_node($node, $tgt_ip, $tgt_mac, $tgt_if, $tgt_os = 'Linux', $tgt_arch = 'x86_64', $slotid, $ipmi_ip, $ipmi_mac, $ipmi_user, $ipmi_pw, $master_if, $master_ipmi_if, $admin_user, $admin_pw) {
 	xcat_node { $node 
 		groups 		=> [ "compute", "all"],
-		ip		=> $private_ip,
-		mac		=> $private_mac,
-		interface	=> $private_if,
+		ip		=> $tgt_ip,
+		mac		=> $tgt_mac,
+		interface	=> $master_if,
 		bmc		=> "${node}-ipmi",
 		bmcusername	=> $ipmi_user,
 		bmcpassword	=> $ipmi_pw,
 		bmcport		=> 0,
 		mgt		=> "ipmi",
 		netboot		=> "pxe",
-		tftpserver	=> $private_servname,
-		nfsserver	=> $private_servname,
+		tftpserver	=> $master_ip,
+		nfsserver	=> $master_ip,
 		nfsdir		=> "/install",
 		installnic	=> $tgt_if,
 		primarynic	=> $tft_if,
-		xcatmaster	=> $private_servname,
+		xcatmaster	=> $master_ip,
 		os		=> $tgt_os,
 		arch		=> $tgt_arch,
 		provmethod	=> "install",
@@ -29,8 +29,8 @@ define vclmgmt::compute_node($node, $private_servname, $private_ip, $private_mac
 	
 	xcat_node { "${node}-ipmi" :
 		groups 		=> [ "ipmi", "all"],
-		ip		=> $ipmi_ip,
-		mac		=> $ipmi_mac,
-		interface	=> $ipmi_if,
+                ip              => $ipmi_ip,
+                mac             => $ipmi_mac,
+                interface	=> $master_ipmi_if,
 	}
 }
