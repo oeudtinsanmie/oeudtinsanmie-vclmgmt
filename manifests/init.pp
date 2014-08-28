1a070daed779223392c21cdffba47f8b36b831b9
Douglas <drmcclus@ncsu.edu>
2014-08-27 21:10:16 -0400

    Update init.pp

--- manifests/init.pp
+++ manifests/init.pp
@@ -15,10 +15,46 @@
 
 class vclmgmt {	
     include xcat
-    include vclmgmt::installfrom, vclmgmt::vcldirs, vclmgmt::cpan, vclmgmt::params, vclmgmt::services, vclmgmt::setup_security, vclmgmt::subversion, vclmgmt::yuminstall
+    include vclmgmt::params
 
-#	if $environment != "root" {
-#		include vclmgmt::mysql
-#	}
-	
+    create_resources(yumrepo, $vclmgmt::params::repos, $vclmgmt::params::defaultrepo)
+    
+    create_resources(package, $vclmgmt::params::pkg_list, { ensure => "latest", provider => "yum", })
+    create_resources(package, $vclmgmt::params::pkg_exclude, { ensure => "absent", })
+    
+    create_resources(vclmgmt::cpan, $vclmgmt::params::cpan_list)
+    
+    file { $vclmgmt::params::vcldir :
+	ensure  => "directory",
+    }
+
+    subversion::checkout { "vcl" :
+	repopath	=> "/repos/asf/vcl/trunk",
+	workingdir	=> $vclmgmt::params::vcldir,
+	host		=> "svn.apache.org",
+	method		=> "http",
+	#revision	=> "",
+    }
+    
+    create_resources(archive, $vclmgmt::params::archives, $vclmgmt::params::webarchive)
+    
+    create_resources(file, $vclmgmt::params::postfiles)
+    
+    create_resources(file, $vclmgmt::params::configs, $vclmgmt::params::configfile)
+    
+    create_resources(vclmgmt::vclcopy, $vclmgmt::params::vclcopyfiles)
+    
+    exec { 'genkeys' :
+	command => '/bin/sh genkeys.sh',
+	cwd	=> $vclmgmt::params::htinc,
+	creates	=> "${vclmgmt::params::htinc}/keys.pem",
+    }
+    
+    create_resources(firewall, $vclmgmt::params::firewalls, $vclmgmt::params::firedefaults)
+    
+    if str2bool("$selinux") {
+    	create_resources(selboolean, $vclmgmt::params::sebools)
+    }
+
+    create_resources(service, $vclmgmt::params::service_list, $vclmgmt::params::servicedefault)
 }
