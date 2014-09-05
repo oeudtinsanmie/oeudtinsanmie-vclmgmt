622eef80d89c712b08f8b6c58470aac60a0809dc
Douglas McClusky <drmcclus@ncsu.edu>
2014-09-05 14:29:16 -0400

    cleaned up vclmgmt defs for getting xcat working w/ Andy
    TODO, but delayed: finish vclresource & vcl_image providers

--- manifests/compute_node.pp
+++ manifests/compute_node.pp
@@ -4,76 +4,96 @@ define vclmgmt::compute_node(
 	$tgt_ip, 
 	$tgt_mac, 
 	$tgt_if, 
-	$tgt_net, 
-	$tgt_domain,
+	$groups = [ "ipmi, "compute", "all" ],
+
+#	$tgt_net, 
+#	$tgt_domain,
+
 	$tgt_os = 'Linux', 
 	$tgt_arch = 'x86_64', 
 	$ipmi_ip, 
 	$ipmi_mac, 
-	$ipmi_net, 
-	$ipmi_domain,
+
+#	$ipmi_net, 
+#	$ipmi_domain,
+
 	$ipmi_user, 
 	$ipmi_pw, 
-	$master_if, 
-	$master_ipmi_if, 
-	$admin_user, 
-	$admin_pw
+	$master_ip,
+
+#	$master_if, 
+#	$master_ipmi_if, 
+
+##	$admin_user, 
+##	$admin_pw,
+
+	$profile,
+	$netboot = 'pxe',
+	$provmethod = 'install'
 ) {
 	xcat_node { $name :
-		groups 		=> [ "ipmi", "compute", "all"],
+		groups 		=> $groups,
 		ip		=> $tgt_ip,
 		mac		=> $tgt_mac,
 		bmc		=>  "${name}-ipmi",
 		bmcusername	=> $ipmi_user,
 		bmcpassword	=> $ipmi_pw,
 		mgt		=> "ipmi",
+                installnic	=> "bootif",
+		primarynic	=> $tgt_if,
+		netboot		=> $netboot,
+		os		=> $tgt_os,
+		profile		=> $profile,
+		provmethod	=> $provmethod,
+		xcatmaster	=> $master_ip,
+		nfsserver	=> $master_ip,
 	}
 	
 	xcat_node {  "${name}-ipmi" :
-		groups 		=> [ "all"],
+		groups 		=> [ "all" ],
                 ip              => $ipmi_ip,
                 mac             => $ipmi_mac,
 	}
 
-	dhcp::hosts { $name:
- 		subnet    => $tgt_net,
-   		hash_data => {
-     			"${name}.${tgt_domain}" => {
-       				interfaces => {
-         				"${master_if}" => $tgt_mac,
-       				}
-     			}
-   		}
-	}
-        
-        dhcp::hosts { "${name}-ipmi":
-                subnet    => $ipmi_net,
-                hash_data => {
-                 	"${name}-ipmi.${ipmi_domain}" => {
-                                interfaces => {
-                                        "${master_ipmi_if}" => $ipmi_mac,
-                                }
-                 	}
-                }
-   	}
+#	dhcp::hosts { $name:
+# 		subnet    => $tgt_net,
+#   		hash_data => {
+#     			"${name}.${tgt_domain}" => {
+#       				interfaces => {
+#         				"${master_if}" => $tgt_mac,
+#       				}
+#     			}
+#   		}
+#	}
+#        
+#        dhcp::hosts { "${name}-ipmi":
+#                subnet    => $ipmi_net,
+#                hash_data => {
+#                 	"${name}-ipmi.${ipmi_domain}" => {
+#                                interfaces => {
+#                                        "${master_ipmi_if}" => $ipmi_mac,
+#                                }
+#                 	}
+#                }
+#   	}
 
 	
         
-        bind::a { $name:
-		ensure => present,
-		zone => $tgt_domain,
-		ptr => false,
-		hash_data => {
-			"${name}" => { owner => $tgt_ip, },
-		}
-   	}
+#        bind::a { $name:
+#		ensure => present,
+#		zone => $tgt_domain,
+#		ptr => false,
+#		hash_data => {
+#			"${name}" => { owner => $tgt_ip, },
+#		}
+#   	}
                 
-        bind::a { "${name}-ipmi":
-                ensure => present,
-                zone =>	$ipmi_domain,
-                ptr => false,
-                hash_data => {
-                        "${name}-ipmi" => { owner => $ipmi_ip, },
-         	}
-        }
+#        bind::a { "${name}-ipmi":
+#                ensure => present,
+#                zone =>	$ipmi_domain,
+#                ptr => false,
+#                hash_data => {
+#                        "${name}-ipmi" => { owner => $ipmi_ip, },
+#         	}
+#        }
 }
--- manifests/xcat_vlan.pp
+++ manifests/xcat_vlan.pp
@@ -5,65 +5,65 @@ define vclmgmt::xcat_vlan(
 	$vlan_alias_ip = undef, 
 	$domain, 
 	$network, 
-	$broadcast, 
+#	$broadcast, 
 	$netmask, 
-	$ip_range, 
-	$vlanid
+#	$ip_range, 
+	$vlanid = undef
 ) {
 
-    if $vlan_alias_ip == undef {
-        $myvlan_alias_ip = $master_ip
-
-	xcat_network { $domain :
-		mgtifname => $master_if,
-		nameservers => $master_ip,
-		dhcpserver => $master_ip,
-		tftpserver => $master_ip,
-		domain => $domain,
-		net => $network,
-		mask => $netmask,
-		dynamicrange => $ip_range,
+    $nethash = {
+	$domain => {
+                mgtifname => $master_if,
+                nameservers => $master_ip,
+                dhcpserver => $master_ip,
+                tftpserver => $master_ip,
+                domain => $domain,
+                net => $network,
+                mask => $netmask,
 	}
     }
+
+    if $vlan_alias_ip == undef {
+        $default_hash = {
+		$domain => {
+		}
+	}
+
     else { 
-        $myvlan_alias_ip = $vlan_alias_ip
 
 	network::if::static { "${master_if}.${vlanid}" :
 		ensure 		=> 'up',
-		ipaddress 	=> $myvlan_alias_ip,
+		ipaddress 	=> $vlan_alias_ip,
 		netmask 	=> $netmask,
 		macaddress 	=> $master_mac,
 		vlan 		=> true,
 		domain 		=> $domain,
 	}
 
-	xcat_network { $domain :
-		mgtifname => $master_if,
-		nameservers => $master_ip,
-		dhcpserver => $master_ip,
-		tftpserver => $master_ip,
-		domain => $domain,
-		net => $network,
-		mask => $netmask,
-		dynamicrange => $ip_range,
-		vlanid => $vlanid,
+	$default_hash = { 
+		$domain => {
+			vlanid => $vlanid,
+		}
 	}
     }
-    notify {"Making dchp subnet: ${network} / ${netmask} -- ${domain}":}
-    dhcp::subnet { $network :
-	broadcast => $broadcast,
-	routers => [ $myvlan_alias_ip, ],
-	netmask => $netmask,
-	domain_name => $domain,
-	other_opts => ['filename "pxelinux.0"', "next-server ${master_ip}"],
-	require => Class['::dhcp::server'],
-    }
 
-    bind::zone { $domain :
-	zone_contact => 'netlabs@help.ncsu.edu',
-  	zone_ns      => $master_ip,
-  	zone_serial  => '2012112901',
-  	zone_ttl     => '604800',
-  	zone_origin  => $domain,
-    }
+    create_resources(xcat_network, $nethash $default_hash)
+
+#    notify {"Making dchp subnet: ${network} / ${netmask} -- ${domain}":}
+#    dhcp::subnet { $network :
+#	broadcast => $broadcast,
+#	routers => [ $myvlan_alias_ip, ],
+#	netmask => $netmask,
+#	domain_name => $domain,
+#	other_opts => ['filename "pxelinux.0"', "next-server ${master_ip}"],
+#	require => Class['::dhcp::server'],
+#    }
+
+#    bind::zone { $domain :
+#	zone_contact => 'netlabs@help.ncsu.edu',
+#  	zone_ns      => $master_ip,
+#  	zone_serial  => '2012112901',
+#  	zone_ttl     => '604800',
+#  	zone_origin  => $domain,
+#    }
 }
