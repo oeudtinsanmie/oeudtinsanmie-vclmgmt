232b4c81e657b76784e14f5b3648f0ec1b95483e
Douglas McClusky <drmcclus@ncsu.edu>
2014-09-09 16:03:38 -0400

    added vcl_computer declaration to compute_node

--- manifests/compute_node.pp
+++ manifests/compute_node.pp
@@ -1,48 +1,58 @@
 include vclmgmt
 
 define vclmgmt::compute_node(
-	$tgt_ip, 
-	$tgt_mac, 
-	$tgt_if, 
-	$groups = [ "ipmi", "compute", "all" ],
-
-#	$tgt_net, 
-#	$tgt_domain,
-
-	$tgt_os = 'Linux', 
-	$tgt_arch = 'x86_64', 
+	$public_ip, 
+	$public_mac,
+	$private_ip, 
+	$private_mac, 
+	$private_if, 
+	$xcat_groups	= [ 'ipmi', 'compute', 'all' ],
+	$vcl_groups 	= [ 'allComputers' ],
+	$tgt_os 		= 'Linux', 
+	$tgt_arch 		= 'x86_64', 
 	$ipmi_ip, 
 	$ipmi_mac, 
-
-#	$ipmi_net, 
-#	$ipmi_domain,
-
 	$ipmi_user, 
 	$ipmi_pw, 
 	$master_ip,
-
-#	$master_if, 
-#	$master_ipmi_if, 
-
-##	$admin_user, 
-##	$admin_pw,
-
 	$profile,
-	$netboot = 'pxe',
-	$provmethod = 'install'
+	$netboot 		= 'pxe',
+	$provmethod 	= 'install'
+	$ram 			= undef,
+	$procnumber 	= undef,
+    $procspeed 		= undef,
+    $network		= undef,
+    $type			= undef,
+    $drivetype		= undef, 
+    $deleted		= undef, 	    
+    $notes			= undef,
+    $location		= undef,
+    $dsa			= undef,
+    $dsapub			= undef,
+    $rsa			= undef,
+    $rsapub			= undef,
+    $hostpub		= undef,
+  	$state			= undef,
+  	$platform		= undef,
+  	$schedule		= undef,
+  	$image			= undef,
+  	$imagerevision	= undef,
+  	$provisioning	= undef,
+  	$vmhost			= undef,
+  	$vmtype			= undef,
 ) {
 	xcat_node { $name :
-		groups 		=> $groups,
-		ip		=> $tgt_ip,
-		mac		=> $tgt_mac,
-		bmc		=>  "${name}-ipmi",
+		groups 		=> $xcat_groups,
+		ip			=> $private_ip,
+		mac			=> $private_mac,
+		bmc			=> "${name}-ipmi",
 		bmcusername	=> $ipmi_user,
 		bmcpassword	=> $ipmi_pw,
-		mgt		=> "ipmi",
-                installnic	=> "bootif",
-		primarynic	=> $tgt_if,
+		mgt			=> "ipmi",
+        installnic	=> "bootif",
+		primarynic	=> $private_if,
 		netboot		=> $netboot,
-		os		=> $tgt_os,
+		os			=> $tgt_os,
 		profile		=> $profile,
 		provmethod	=> $provmethod,
 		xcatmaster	=> $master_ip,
@@ -54,46 +64,34 @@ define vclmgmt::compute_node(
                 ip              => $ipmi_ip,
                 mac             => $ipmi_mac,
 	}
-
-#	dhcp::hosts { $name:
-# 		subnet    => $tgt_net,
-#   		hash_data => {
-#     			"${name}.${tgt_domain}" => {
-#       				interfaces => {
-#         				"${master_if}" => $tgt_mac,
-#       				}
-#     			}
-#   		}
-#	}
-#        
-#        dhcp::hosts { "${name}-ipmi":
-#                subnet    => $ipmi_net,
-#                hash_data => {
-#                 	"${name}-ipmi.${ipmi_domain}" => {
-#                                interfaces => {
-#                                        "${master_ipmi_if}" => $ipmi_mac,
-#                                }
-#                 	}
-#                }
-#   	}
-
 	
-        
-#        bind::a { $name:
-#		ensure => present,
-#		zone => $tgt_domain,
-#		ptr => false,
-#		hash_data => {
-#			"${name}" => { owner => $tgt_ip, },
-#		}
-#   	}
-                
-#        bind::a { "${name}-ipmi":
-#                ensure => present,
-#                zone =>	$ipmi_domain,
-#                ptr => false,
-#                hash_data => {
-#                        "${name}-ipmi" => { owner => $ipmi_ip, },
-#         	}
-#        }
+	vcl_computer { $name :
+		hostname 		=> $name,
+		public_ip 		=> $public_ip,
+		private_ip		=> $private_ip,
+		public_ip 		=> $public_mac,
+		private_ip		=> $private_mac,
+		ram				=> $ram,
+		procnumber		=> $procnumber,
+        procspeed		=> $procspeed,
+        network			=> $network,
+        type 			=> $type,
+        drivetype   	=> $drivetype,      
+        deleted         => $deleted, 	    
+        notes           => $notes, 	        
+        location        => $location,       
+        dsa             => $dsa,            
+        dsapub          => $dsapub,         
+        rsa             => $rsa,            
+        rsapub          => $rsapub,         
+        hostpub         => $hostpub,        
+      	state           => $state, 		        
+      	platform        => $platform,         
+      	schedule        => $schedule,         
+      	image           => $image,            
+      	imagerevision	=> $imagerevision,    
+      	provisioning    => $provisioning,     
+      	vmhost        	=> $vmhost,
+      	vmtype        	=> $vmtype,
+	}
 }
--- manifests/xcat_pod.pp
+++ manifests/xcat_pod.pp
@@ -6,29 +6,8 @@ define vclmgmt::xcat_pod(
 ) {
 	ensure_resource(vclmgmt::xcat_vlan, $name, $private_hash)
 	ensure_resource(vclmgmt::xcat_vlan, "${name}-ipmi", $ipmi_hash)
-#	
-#	if $private_hash[vlanid] == undef {
-#		$private_if = $private_hash[master_if]
-#	}
-#	else {
-#		$private_if = "${private_hash[master_if]}.${private_hash[vlanid]}"
-#	}
-#	
-#	if $ipmi_hash[vlanid] == undef {
-#		$ipmi_if = $ipmi_hash[master_if]
-#	}
-#	else {
-#		$ipmi_if = "${ipmi_hash[master_if]}.${ipmi_hash[vlanid]}"
-#	}
-#
-	$tmphash = {
-#		tgt_net 	=> $private_hash[network],
-#                tgt_domain	=> $private_hash[domain],
-#		ipmi_net 	=> $ipmi_hash[network],
-#                ipmi_domain     => $ipmi_hash[domain],
-#		master_if	=> $private_if,
-#		master_ipmi_if	=> $ipmi_if,
 
+	$tmphash = {
 		master_ip	=> $private_hash[master_ip],
 	}
 
